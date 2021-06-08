52d0b326dd6b0f426c026b71c9ecce04b8600e3c
La-wii <sentilavale@gmail.com>
2021-06-08 20:40:34 +0200

    pt 2 array utente

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var numPc = [];
 
 while (numPc.length < 16) {
     var randomNum = randomNumbers(1, 100)
+
     if (!numPc.includes(randomNum)) { 
         numPc.push(randomNum);
     }
@@ -12,19 +13,44 @@ while (numPc.length < 16) {
 console.log(numPc)
 
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
+// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
 
-// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero. 
+var arrayUtente = [];
+var lose = false
 
-// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+while (arrayUtente.length < 16 && !lose) {
+    var numero
+    var numInserito = parseInt(prompt('Inserisci numero'));
 
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+    if (!numPc.includes(numInserito)) {
+        console.log('num ok');
 
+        if (!arrayUtente.includes(numInserito)) {
+            arrayUtente.push(numInserito);
+        } else {
+            console.log('num già inserito');
+            alert('Hai già inserito questo numero' + arrayUtente)
+        }
+
+    } else {
+        var lose = true;
+        console.log('hai perso');
+        alert('Sei morto :(')
+    }
+    
+    console.log(arrayUtente);
+}
+
+console.log(arrayUtente.length);
 
 
 
 // -------------------- Funzioni -----------------------------
 
 // funzione numero random
+
 function randomNumbers(numMin, numMax) {
     return Math.floor(Math.random() * (numMax - numMin + 1) ) + numMin;
 }
+
+
